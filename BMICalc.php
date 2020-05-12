92c447520ea6f44b61f6f4d4bd878e947c5837e6
sanjanasenthil <51441687+sanjanasenthil@users.noreply.github.com>
2020-05-13 02:27:26 +1000

    Add files via upload

---
+++ BMICalc.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>BMI Calculator</title>
+  <link rel="stylesheet" href="style.css"
+</head>
+
+<body>
+  <a href="calendar.php">Calendar</a>
+  <a href="notifications.php">Notifications</a>
+  <a href="BMICalc.php">BMI Calculator</a>
+  <a href="search.php">Search</a>
+  <a href="help.php">Help</a>
+  <a href="signup.php">Sign Up</a>
+  <a href="login.php">Login</a>
+
+  <div id="logo">
+  <h1> BMI Calculator <h/1>
+  </div>
+
+</body>
+
+</html>
---
+++ search.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Search</title>
+  <link rel="stylesheet" href="style.css"
+</head>
+
+<body>
+  <a href="calendar.php">Calendar</a>
+  <a href="notifications.php">Notifications</a>
+  <a href="BMICalc.php">BMI Calculator</a>
+  <a href="search.php">Search</a>
+  <a href="help.php">Help</a>
+  <a href="signup.php">Sign Up</a>
+  <a href="login.php">Login</a>
+
+  <div id="logo">
+  <h1> Search <h/1>
+  </div>
+
+</body>
+
+</html>
